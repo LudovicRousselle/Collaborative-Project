b64cd718088e5c3a0717de199335b18e55f6d6bf
Geoffroy Bordenave* <gbordenave@artfx.fr>
2022-04-04 12:48:17 +0200

    ta gueule corentin

---
+++ Assets/Scripts/GPE/Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Button : InteractableObject
+{
+    [SerializeField] RewindableObject target = default;
+
+    public override void OnInteract()
+    {
+        target.OnInteract();
+    }
+}
---
+++ Assets/Scripts/GPE/RewindableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class RewindableObject : InteractableObject
+{
+    private void OnButtonInteract()
+    {
+        
+    }
+}
