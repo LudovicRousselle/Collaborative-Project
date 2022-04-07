df7d292e3114449b4a94af841f3a7b417ef64cfc
Coccitech <98786739+Coccitech@users.noreply.github.com>
2022-04-07 17:04:11 +0200

    mouving platefom on build

--- Assets/Scripts/GPE/MouvingPlatform.cs
+++ Assets/Scripts/GPE/MouvingPlatform.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class MouvingPlatform : MonoBehaviour
 {
+    [SerializeField] private Transform movingPlatform;
+    [SerializeField] private Transform position1;
+    [SerializeField] private Transform position2;
+    [SerializeField] private Vector3 newPosition;
+    [SerializeField] private string currentsState;
+    [SerializeField] private float smooth;
+    [SerializeField] private float resetTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChangeTarget();
     }
-
-    // Update is called once per frame
-    void Update()
+   
+    private void FixedUpdate()
     {
-        
+        movingPlatform.position = Vector3.Lerp(movingPlatform.position, newPosition, smooth * Time.deltaTime);
     }
+    private void ChangeTarget()
+    {
+        if (currentsState == "MtP1")
+        {
+            currentsState = "MtP2";
+            newPosition = position2.position;
+        }
+        else if (currentsState == "MtP2")
+        {
+            currentsState = "MtP1";
+            newPosition = position1.position;
+        }
+        else if (currentsState == "")
+        {
+            currentsState = "MtP2";
+            newPosition = position2.position;
+        }
+        Invoke("ChangeTarget", resetTime);
+    }
+    
 }
