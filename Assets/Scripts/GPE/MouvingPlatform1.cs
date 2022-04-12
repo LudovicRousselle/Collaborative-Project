9f13340b4508cf642700bc006df06f1e20517d72
Coccitech <98786739+Coccitech@users.noreply.github.com>
2022-04-12 17:54:36 +0200

    mouving platfom

--- Assets/Scripts/GPE/MouvingPlatform1.cs
+++ Assets/Scripts/GPE/MouvingPlatform1.cs
@@ -7,17 +7,29 @@ public class MouvingPlatform1 : MonoBehaviour
     [SerializeField] GameObject[] waypoints;
     int currentWaypointIndex = 0;
     [SerializeField] float speed = 1f;
+    [SerializeField] float timer;
+    [SerializeField] float timeReset;
 
     void Update()
     {
         if(Vector3.Distance(transform.position,waypoints[currentWaypointIndex].transform.position)<.1f)
         {
-            currentWaypointIndex++;
-            if(currentWaypointIndex>=waypoints.Length)
+            
+            timer -= Time.deltaTime;
+            if (timer<=0)
             {
-                currentWaypointIndex = 0;
+                currentWaypointIndex=currentWaypointIndex+1;
+                timer = timeReset;
+                if (currentWaypointIndex >= waypoints.Length)
+                    {
+                        currentWaypointIndex = 0;
+                        
+                    }
+                        
             }
+            
         }
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
+       
     }
 }
