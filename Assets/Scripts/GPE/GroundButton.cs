0665b4b5d66c33fd56eb56365cf5bc8621c1fb17
Coccitech <98786739+Coccitech@users.noreply.github.com>
2022-05-13 10:14:51 +0200

    button

--- Assets/Scripts/GPE/GroundButton.cs
+++ Assets/Scripts/GPE/GroundButton.cs
@@ -7,23 +7,29 @@ public class GroundButton : MonoBehaviour
     public bool isPress= false;
     public float buttonDown = 0.12f;
     public float buttonUp = 0.06f;
-
+    public static bool  onPlatform = false;
+   
     private void OnCollisionStay(Collision collision)
-    {
-        if ((collision.gameObject.tag == "SBlock"))
-        {
-            isPress = true;
-            Debug.Log("touch");
-            transform.position += new Vector3(0, - buttonDown, 0 ) * Time.deltaTime;
+    {           
+            if ((collision.gameObject.tag == "Rewindable"))
+            {
+                
+                isPress = true;
+                onPlatform = true;
+                Debug.Log("touch true");
+                transform.position += new Vector3(0, -buttonDown, 0) * Time.deltaTime;
 
-        }
-        
+            } 
+
+
+
     }
     private void OnTriggerExit(Collider other)
     {
-        if ((other.gameObject.tag == "SBlock"))
+        if ((other.gameObject.tag == "Rewindable"))
         {
             isPress = false;
+            onPlatform = false;
             Debug.Log("touch");
             transform.position -= new Vector3(0, -buttonDown, 0) * Time.deltaTime;
         }
--- Assets/Scripts/GPE/MouvingPlatform1.cs
+++ Assets/Scripts/GPE/MouvingPlatform1.cs
@@ -6,55 +6,66 @@ public class MouvingPlatform1 : RewindableObject
 {
     [SerializeField] GameObject[] waypoints;
     int currentWaypointIndex = 0;
-    [SerializeField] float speed = 1f;
-    [SerializeField] float timer;
-    [SerializeField] float timeReset;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float timer;
+    [SerializeField] private float timeReset;
+    
     public bool isRewind;
-
+   
 
     void Update()
     {
-        if (Vector3.Distance(transform.position,waypoints[currentWaypointIndex].transform.position)<.1f)
-        {
-            
-            timer -= Time.deltaTime;
-            if (timer<=0)
-            {
-                currentWaypointIndex=currentWaypointIndex+1;
-                timer = timeReset;
-                if (currentWaypointIndex >= waypoints.Length)
-                {
-                        currentWaypointIndex = 0;
-                        
 
-                }
-                        
-            }
-            
-        }
-        else if (isRewind)
+        
+
+        if (GroundButton.onPlatform==false)
         {
+            Debug.Log(GroundButton.onPlatform);
+            
             if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f)
             {
 
                 timer -= Time.deltaTime;
                 if (timer <= 0)
                 {
-                    currentWaypointIndex = currentWaypointIndex - 1;
+                    currentWaypointIndex = currentWaypointIndex + 1;
                     timer = timeReset;
-                    if (currentWaypointIndex <= waypoints.Length)
+                    if (currentWaypointIndex >= waypoints.Length)
                     {
-                        currentWaypointIndex = 2;
-                        isRewind = false;
+                        currentWaypointIndex = 0;
+
 
                     }
 
                 }
 
             }
+            else if (isRewind)
+            {
+                if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f)
+                {
+
+                    timer -= Time.deltaTime;
+                    if (timer <= 0)
+                    {
+                        currentWaypointIndex = currentWaypointIndex - 1;
+                        timer = timeReset;
+                        if (currentWaypointIndex <= waypoints.Length)
+                        {
+                            currentWaypointIndex = 2;
+                            isRewind = false;
+
+                        }
+
+                    }
+
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
+            isRewind = false;
         }
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
-        isRewind = false;
+        
+       
     }
     //override protected void OnRewind()
     //{
