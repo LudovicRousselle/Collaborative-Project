cb1a2ed570b6cfddd17868ff562628e2f1c088c4
LudovicRousselle <lrousselle@artfx.fr>
2022-05-29 02:07:36 +0200

    Main Menu Script

---
+++ Assets/Scripts/Menu/MenuManagement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagement : MonoBehaviour
+{
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OptionButton()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
