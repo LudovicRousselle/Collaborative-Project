659ec34c56cde4f10cbfa94e61ccaa0f19bd19e1
Corentin Paya <cpaya@free.fr>
2022-05-30 16:46:12 +0200

    Config Fades

--- Assets/Scripts/Manager/SaveManager.cs
+++ Assets/Scripts/Manager/SaveManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveManager : MonoBehaviour
 {
@@ -29,6 +30,10 @@ public class SaveManager : MonoBehaviour
 
     private IEnumerator m_coroutine;
 
+    //Transitions
+    [SerializeField] private Image m_transition;
+    public bool m_Fading = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -46,10 +51,23 @@ public class SaveManager : MonoBehaviour
                 StartCoroutine(m_coroutine);
             }
         }
+
+        //If the toggle returns true, fade in the Image
+        if (m_Fading == true)
+        {
+            //Fully fade in Image (1) with the duration of 2
+            m_transition.CrossFadeAlpha(1, .5f, false);
+        }
+        //If the toggle is false, fade out to nothing (0) the Image with a duration of 2
+        if (m_Fading == false)
+        {
+            m_transition.CrossFadeAlpha(0, .5f, false);
+        }
     }
 
     IEnumerator ReloadScene()
     {
+        m_Fading = true;
         yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Debug.Log("Reloaded");
